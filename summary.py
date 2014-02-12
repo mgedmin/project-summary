20ac933e88d2ac988b7bc07f42a1a01af107bddd
Marius Gedminas <marius@gedmin.as>
2014-02-12 22:17:43 +0000

    Refactoring the Project class and get_projects()

--- summary.py
+++ summary.py
@@ -65,25 +65,35 @@ def get_pending_commits(repo_path, last_tag):
 
 class Project(object):
 
-    def __init__(self, url, last_tag, last_tag_date, pending_commits):
+    def __init__(self, working_tree, url, last_tag, last_tag_date,
+                 pending_commits):
+        self.working_tree = working_tree
         self.url = url
         self.last_tag = last_tag
         self.last_tag_date = last_tag_date
         self.pending_commits = pending_commits
 
+    @classmethod
+    def from_working_tree(cls, working_tree):
+        url = normalize_github_url(get_repo_url(working_tree))
+        last_tag = get_last_tag(working_tree)
+        last_tag_date = get_date_of_tag(working_tree, last_tag)
+        pending_commits = get_pending_commits(working_tree, last_tag)
+        return cls(working_tree, url, last_tag, last_tag_date, pending_commits)
+
     @property
     def name(self):
         return get_project_name(self.url)
 
+    @property
+    def compare_url(self):
+        return '{base}/compare/{tag}...master'.format(base=self.url,
+                                                      tag=self.last_tag)
+
 
 def get_projects():
-    repos = get_repos()
-    for repo in repos:
-        url = normalize_github_url(get_repo_url(repo))
-        last_tag = get_last_tag(repo)
-        last_tag_date = get_date_of_tag(repo, last_tag)
-        pending_commits = get_pending_commits(repo, last_tag)
-        yield Project(url, last_tag, last_tag_date, pending_commits)
+    for path in get_repos():
+        yield Project.from_working_tree(path)
 
 
 def main():
