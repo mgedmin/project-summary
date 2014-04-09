0988723de236dbf4446cd710dc4c98be3a67877a
Marius Gedminas <marius@gedmin.as>
2014-04-09 10:17:03 +0300

    Lazily compute last_tag et al
    Should prevent "fatal: No names found, cannot describe anything" errors
    for ignored projects.

--- summary.py
+++ summary.py
@@ -36,6 +36,16 @@ def pipe(*cmd, **kwargs):
     return p.communicate()[0]
 
 
+class reify(object):
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __get__(self, obj, cls=None):
+        value = self.fn(obj)
+        obj.__dict__[self.fn.__name__] = value
+        return value
+
+
 #
 # Data extraction
 #
@@ -81,21 +91,24 @@ def get_pending_commits(repo_path, last_tag):
 
 class Project(object):
 
-    def __init__(self, working_tree, url, last_tag, last_tag_date,
-                 pending_commits):
+    def __init__(self, working_tree):
         self.working_tree = working_tree
-        self.url = url
-        self.last_tag = last_tag
-        self.last_tag_date = last_tag_date
-        self.pending_commits = pending_commits
 
-    @classmethod
-    def from_working_tree(cls, working_tree):
-        url = normalize_github_url(get_repo_url(working_tree))
-        last_tag = get_last_tag(working_tree)
-        last_tag_date = get_date_of_tag(working_tree, last_tag)
-        pending_commits = get_pending_commits(working_tree, last_tag)
-        return cls(working_tree, url, last_tag, last_tag_date, pending_commits)
+    @reify
+    def url(self):
+        return normalize_github_url(get_repo_url(self.working_tree))
+
+    @reify
+    def last_tag(self):
+        return get_last_tag(self.working_tree)
+
+    @reify
+    def last_tag_date(self):
+        return get_date_of_tag(self.working_tree, self.last_tag)
+
+    @reify
+    def pending_commits(self):
+        return get_pending_commits(self.working_tree, self.last_tag)
 
     @property
     def owner(self):
@@ -127,7 +140,7 @@ class Project(object):
 
 def get_projects():
     for path in get_repos():
-        p = Project.from_working_tree(path)
+        p = Project(path)
         if p.name not in IGNORE:
             yield p
 
