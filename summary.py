3c21cbb5fbabd6e847787d5298212c0dcfc281c4
Marius Gedminas <marius@gedmin.as>
2018-11-27 20:31:28 +0200

    Suppress warnings from running setup.py --classifiers

--- summary.py
+++ summary.py
@@ -228,7 +228,8 @@ def get_pending_commits(repo_path, last_tag, branch='master'):
 
 
 def get_supported_python_versions(repo_path):
-    classifiers = pipe(sys.executable, "setup.py", "--classifiers", cwd=repo_path).splitlines()
+    classifiers = pipe(sys.executable, "setup.py", "--classifiers",
+                       cwd=repo_path, stderr=subprocess.PIPE).splitlines()
     prefix = 'Programming Language :: Python :: '
     impl_prefix = 'Programming Language :: Python :: Implementation :: '
     cpython = impl_prefix + 'CPython'
