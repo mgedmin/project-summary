5b951b3fa7062b2b612d52f284a573866cc13ad3
Marius Gedminas <marius@gedmin.as>
2021-10-15 22:14:19 +0300

    Python 3.10 is out

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         'Private :: Do Not Upload To PyPI',  # it rejects unknown classifiers ;)
